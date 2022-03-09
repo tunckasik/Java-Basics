d716a6714131a7b77f128fa5f1f5b085956c8523
tunckasik <tunckasikali@gmail.com>
2022-03-09 08:11:27 -0800

    Yeni eklenenler

---
+++ AccessModifierlar/src/com/tunckasik/paket1/At.java
@@ -0,0 +1,18 @@
+
+package com.tunckasik.paket1;
+import com.tunckasik.paket2.Hayvan;
+
+
+public class At extends Hayvan{
+    
+    public At(String isim) {
+        super(isim);
+    }
+    public void ismini_soyle() {
+        
+        System.out.println(super.isim);
+        
+    }
+    
+    
+}
---
+++ AccessModifierlar/src/com/tunckasik/paket2/Araba.java
@@ -0,0 +1,9 @@
+
+package com.tunckasik.paket2;
+
+public class Araba {
+    String model = "Renault";
+    String renk = "Gümüş";
+    String yil = "2001";
+    
+}
