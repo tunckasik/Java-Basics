8591ff7727093229d1139543d3e15d134f932dba
Ali TUNCKASIK <tunckasikali@gmail.com>
2022-04-22 10:13:36 -0700

    Update Main.java

--- Degiskenler/src/Main.java
+++ Degiskenler/src/Main.java
@@ -9,14 +9,20 @@ public class Main {
       */
      int a = 100;
              
-     int b = 25+ a;
+     int b = 25 + a;
      
      int c = a + b - 50;
      
      int Toplam = 2*a + b - c/2;
      
         System.out.println("Islemin sonucu " + Toplam + " dur");
-                 
+
+     int M;
+     int N;
+     M = 5;
+     N = M+1;
+     int MN = M+N;
+        System.out.println(MN);
            
     }
     
